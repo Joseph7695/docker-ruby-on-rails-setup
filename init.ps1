a6573087754ae350eb046dbd44a80909043c9d6f
Joseph <jtfq7695@gmail.com>
2021-12-03 13:54:16 +0800

    replace after first run ?

--- init.ps1
+++ init.ps1
@@ -1,3 +1,5 @@
 docker-compose run --no-deps web rails new . --force --database=postgresql
-docker-compose build
+# docker-compose build
+Rename-Item -Path "prod_docker-compose.yml" -NewName "docker-compose.yml"
+Remove-Item docker-compose.yml
 Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
